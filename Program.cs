f73078c0adbe25341e6ff30bdf052d28502aa405
Beatrix Devanti <beatrix.devanti.ft21@mail.umy.ac.id>
2022-11-18 15:37:43 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace doublelinkedlist
+{
+    class Node
+    {
+        /*Node class represents the node of doubly linked list.
+         * Its consists of the information part and links to
+         * it succedding and preceeding nodes
+         * in terms of next and previous node.*/
+
+        
+    }
+        
+}
