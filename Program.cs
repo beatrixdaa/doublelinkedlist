736f0e8cdd396eb83a64cce17fb3030064c5be04
Beatrix Devanti <beatrix.devanti.ft21@mail.umy.ac.id>
2022-11-18 15:38:07 +0700

    add class node

--- Program.cs
+++ Program.cs
@@ -13,7 +13,10 @@ namespace doublelinkedlist
          * it succedding and preceeding nodes
          * in terms of next and previous node.*/
 
-        
+        public int rollNumber;
+        public string name;
+        public Node text;/*points to the succeeding node*/
+        public Node prev;/*points to the preceeding node*/
     }
         
 }
